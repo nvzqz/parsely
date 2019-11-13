bef9cb8f495f8399a7f1a3b97ba93000a2db3327
Nikolai Vazquez <nikvzqz@gmail.com>
2019-11-13 05:51:20 +0100

    [Cargo] Create library

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Parsing primitives.
+//!
+//! This crate is under construction. Contact
+//! [Nikolai Vazquez](https://twitter.com/nikolaivazquez) if you'd like to help.
+
+#![cfg_attr(not(feature = "std"), no_std)]
